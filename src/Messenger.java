111a0e2e5cf5691d3244d3c2488aceb4f7303f25
Vahe Odabashyan <vaheodabashyan@gmail.com>
2019-12-15 21:31:19 +0400

    add more stuff

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,6 +1,6 @@
 public class Messenger {
     private String message;
     private int time;
-    private String senderID;
-    private String receiverId;
+    private String sender;
+    private String receiver;
 }
